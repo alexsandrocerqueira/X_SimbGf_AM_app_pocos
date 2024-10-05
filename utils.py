8ebafd3fc7def6e04fc21beb84c8c58633196365
Alexsandro <alexsandrocerqueira@ufba.br>
2024-10-05 17:43:12 -0300

    cluster_analysis

--- utils.py
+++ utils.py
@@ -101,3 +101,57 @@ def plot_well_logs_comparison(df, well,lithology_col,predicted_col,colors,lithol
 
     return fig, axes
 
+
+def plot_well_logs_comparison_cluster(df, well,lithology_col,predicted_col,colors,lithology,dz=100,figsize=(11,12)):
+    fig, axes = plt.subplots(1, 6, figsize=figsize)
+
+    # Definindo o limite vertical e adicionando grade
+    for ax in axes:
+        ax.set_ylim(df[df.WELL == well].DEPTH_MD.max() + dz, df[df.WELL == well].DEPTH_MD.min() - dz)
+        ax.grid()
+
+    # Primeiro gráfico: GR com CALI
+    axes[0].plot(df[df.WELL == well].GR, df[df.WELL == well].DEPTH_MD, color='blue', lw=0.8, label='GR')
+    ax0 = axes[0].twiny()
+    ax0.plot(df[df.WELL == well].CALI, df[df.WELL == well].DEPTH_MD, c='r', ls='--', lw=0.8, label='CALI')
+    ax0.legend(loc='upper left', fontsize=8)
+
+    # Segundo gráfico: RDEP e RMED
+    axes[1].plot(df[df.WELL == well].RDEP, df[df.WELL == well].DEPTH_MD, color='blue', lw=0.8, label='RILD')
+    axes[1].plot(df[df.WELL == well].RMED, df[df.WELL == well].DEPTH_MD, color='red', lw=0.8, label='RILM', ls='--')
+    axes[1].set_xscale('log')
+
+    # Terceiro gráfico: DTC
+    axes[2].plot(df[df.WELL == well].DTC, df[df.WELL == well].DEPTH_MD, color='blue', lw=0.8, label='DTC')
+
+    # Quarto gráfico: RHOB com NPHI
+    axes[3].plot(df[df.WELL == well].RHOB, df[df.WELL == well].DEPTH_MD, color='blue', lw=0.8, label='RHOB')
+    ax3 = axes[3].twiny()
+    ax3.plot(df[df.WELL == well].NPHI, df[df.WELL == well].DEPTH_MD, color='red', lw=0.8, label='NPHI', ls='--')
+
+
+    for i,j in zip(lithology,colors):
+
+        axes[4].fill_betweenx(df[df.WELL == well].DEPTH_MD, 0,
+                           1, 
+                           where=(df[df.WELL == well][lithology_col] == i), 
+                           color=j, alpha=1,label=i)
+        
+    for i,j in zip(df[df.WELL == well][predicted_col].unique(),colors):
+
+        axes[5].fill_betweenx(df[df.WELL == well].DEPTH_MD, 0,
+                           1, 
+                           where=(df[df.WELL == well][predicted_col]== i), 
+                           color=j, alpha=1,label=i)
+
+    # Adicionando legendas em todos os gráficos
+    for ax in axes:
+        ax.legend(fontsize=8, loc='upper right')
+
+    axes[4].legend(fontsize=5)
+    axes[5].legend(fontsize=5)
+
+    # Ajustando o layout para evitar sobreposição
+    fig.tight_layout()
+
+    return fig, axes
